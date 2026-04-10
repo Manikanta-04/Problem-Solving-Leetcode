d520f410e16895b7929256de0023b28e1f2d3b35
Manikanta-04 <238w1a12a7@vrsec.ac.in>
2026-04-10 22:32:52 +0530

    solved the Length of Linked List problem

---
+++ Linked List/Length of Linked List.java
@@ -0,0 +1,20 @@
+/*
+Definition for Linked List
+class Node{
+    int data;
+    Node next;
+    Node(int a){  data = a; next = null; }
+}
+*/
+class Solution {
+    public int getCount(Node head) {
+        // code here
+        int cnt=0;
+        Node temp=head;
+        while(temp.next!=null){
+            cnt++;
+            temp=temp.next;
+        }
+        return cnt+1;
+    }
+}
