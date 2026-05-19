9e287e053e044ce1e2d40f6ac0f69cfb9d36d36e
Manikanta-04 <238w1a12a7@vrsec.ac.in>
2026-05-19 15:17:02 +0530

    2540. Minimum Common Value solved

---
+++ Basic Hashing/2540. Minimum Common Value.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+  HashSet<Integer> set=new HashSet<>();
+  int min=Integer.MAX_VALUE;
+  for(int i : nums1){
+    set.add(i);
+  }
+  for(int i : nums2){
+    if(set.contains(i) && i<min) min=i;
+  }
+  if(min==Integer.MAX_VALUE) return -1;
+  return min;
+    }
+}
