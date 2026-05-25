9c50dd0dd6b892075a303dd0eab86742d581aead
Manikanta-04 <238w1a12a7@vrsec.ac.in>
2026-05-25 21:19:36 +0530

    3591. Check if Any Element Has Prime Frequency solved

---
+++ Basic Hashing/3591. Check if Any Element Has Prime Frequency.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public boolean checkPrimeFrequency(int[] nums) {
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int x : nums){
+            map.put(x, map.getOrDefault(x,0)+1);
+        }
+
+        for(int freq : map.values()){
+
+            if(isPrime(freq)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+    public boolean isPrime(int n){
+
+        if(n < 2){
+            return false;
+        }
+
+        for(int i=2; i*i<=n; i++){
+
+            if(n % i == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
