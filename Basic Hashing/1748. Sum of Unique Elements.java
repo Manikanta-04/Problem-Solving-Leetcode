096058b450f21904b8a0c4ba24dd065c994fcd9d
Manikanta-04 <238w1a12a7@vrsec.ac.in>
2026-06-06 19:57:47 +0530

    1748. Sum of Unique Elements solved

---
+++ Basic Hashing/1748. Sum of Unique Elements.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+     HashMap<Integer,Integer> map=new HashMap<>();
+     int s=0;
+     for(int i : nums){
+        map.put(i,map.getOrDefault(i,0)+1);
+     } 
+     for(int i : map.keySet()){
+        if(map.get(i)==1) s+=i;
+     }  
+     return s;
+    }
+}
