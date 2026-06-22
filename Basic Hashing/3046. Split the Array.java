bd4a12737af924729af481b47970486dc173a69b
Manikanta-04 <238w1a12a7@vrsec.ac.in>
2026-06-22 20:23:24 +0530

    3046. Split the Array solved

---
+++ Basic Hashing/3046. Split the Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPossibleToSplit(int[] nums) {
+       HashMap<Integer,Integer> map=new HashMap<>();
+       for(int i : nums ){
+        map.put(i,map.getOrDefault(i,0)+1);
+       }
+       for(int key: map.keySet()){
+        if(map.get(key)>2) return false;
+       }
+       return true; 
+    }
+}
